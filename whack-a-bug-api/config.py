9393060055055a22aa136e16da5dc3ec2b173e79
loverofMush <tolujamal1@gmail.com>
2021-01-05 22:24:21 +0100

    - added config variables

---
+++ whack-a-bug-api/config.py
@@ -0,0 +1,30 @@
+import os
+
+class BaseConfig:
+    """ Default app configuration"""
+    
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    DEBUG = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JSON_SORT_KEYS = False
+    
+    
+class DevelopmentConfig(BaseConfig):
+    """App configuration in development mode"""
+    
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    
+class ProductionConfig(BaseConfig):
+    """App configuration in production mode"""
+    
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    
+class TestConfig(BaseConfig):
+    """App cofiguration in test mode"""
+    
+    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE')
+    TESTING = True
+    DEBUG = True
+    HASH_ROUNDS = 1
+    PRESERVE_CONTEXT_ON_EXCEPTION = False
