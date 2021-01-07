4210f1ad7fced41bb40e7d1a54ae5b16663cb2d4
loverofMush <tolujamal1@gmail.com>
2021-01-07 16:26:52 +0100

    - added request methods to route - check request.method in function

--- whack_a_bug_api/routes/auth_routes/auth_routes.py
+++ whack_a_bug_api/routes/auth_routes/auth_routes.py
@@ -1,8 +1,39 @@
-from flask import jsonify
+from flask import jsonify, request
 from whack_a_bug_api.models.projects import Project
 from . import auth
 
 
-@auth.route('/')
+@auth.route('/projects', methods=['GET', 'POST'])
 def getProjects():
-    return Project.get_all()
+    if request.method == 'POST':
+        title = request.form.get('title')
+                    
+        if title:
+            project = Project(title=title)
+            project.save()
+            
+            res = jsonify({
+                'id': project.id,
+                'title': project.title,
+                'created_on': project.created_on,
+                'modified_on': project.modified_on 
+            })
+            res.status_code = 201
+            return res
+            
+    else:
+        projects = Project.get_all()
+        results = []
+        
+        for project in projects:
+            obj = {
+                'id': project.id,
+                'title': project.title,
+                'created_on': project.created_on,
+                'modified_on': project.modified_on
+            }
+            results.append(obj)
+        
+        res = jsonify(results)
+        res.status_code = 200
+        return res
