5b028c6e9df400df6b4db689f60059f6756902d2
loverofMush <tolujamal1@gmail.com>
2021-01-07 16:32:59 +0100

    - test get and post methods in projects route

---
+++ whack_a_bug_api/tests/projects/test_project.py
@@ -0,0 +1,23 @@
+from whack_a_bug_api.tests.BaseCase import BaseCase
+from whack_a_bug_api.models.projects import Project
+
+
+class ProjectTests(BaseCase):
+    def test_projects_can_be_created(self):
+        self.project = {'title': 'Food Blog Design'}
+        
+        with self.client:
+            res = self.client.post('/projects', data = self.project)
+            self.assertEqual(res.status_code, 201)
+            self.assertIn('Food Blog', str(res.data))
+            
+    def test_api_gets_all_projects(self):
+        self.project = {'title': 'Food Blog Design'}
+        
+        with self.client:
+            res = self.client.post('/projects', data = self.project)
+            self.assertEqual(res.status_code, 201)
+            
+            res = self.client.get('/projects')
+            self.assertEqual(res.status_code, 200)
+            self.assertIn('Food Blog', str(res.data))
