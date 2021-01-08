e9452c60f7ae99d1461c78f4cd0b8ecc61ca65f9
loverofMush <tolujamal1@gmail.com>
2021-01-09 00:06:38 +0100

    - created new class view for PUT/GET_BY_ID - added delete method in ProjectsView

--- whack_a_bug_api/routes/main_routes/main_routes.py
+++ whack_a_bug_api/routes/main_routes/main_routes.py
@@ -59,102 +59,75 @@ class ProjectsView(MethodView):
         else:
             data = {'message': 'Title is required!'}
             return make_response(jsonify(data), 400)
+        
+    def delete(self):
+        ids = request.json['selectedIDs']
+        data = Project.delete(ids)
+        
+        return make_response(jsonify(data), 200)
 
+class SingleProjectView(MethodView):
+    """Class controlling routes for editing and updating projects"""
+    
+    def get(self, id):
+        project = Project.query.filter_by(id = id).first()
+        
+        if project is None:
+            abort(404)
+            
+        data = {}
+        data['id'] = project.id
+        data['title'] = project.title
+        data['description'] = project.description
+        data['created_on'] = project.created_on
+        
+        res = {
+            'data': data
+        }
+        return make_response(jsonify(res), 200)
+    
+    def put(self, id):
+        form_data = request.get_json()
+        project = Project.query.filter_by(id = id).first()
+        
+        if project is None:
+            abort(404)
+            
+        try:
+            project.title = form_data.get('title')
+            project.description = form_data.get('description')
+            project.save()
+            
+            data = {}
+            data['id'] = project.id
+            data['title'] = project.title
+            data['created_on'] = project.created_on
+            
+            res = {
+                'data': data,
+                'status': 'success',
+                'message': 'Project updated successfully!'
+            }
+            
+            return make_response(jsonify(res), 200)
+        except Exception as e:
+            res = {'message': str(e)}
+            return make_response(jsonify(res), 401)
+        
+        
 #define the API resources
 projects_view = ProjectsView.as_view('projects_api')
+single_project_view = SingleProjectView.as_view('single_project_view')
 
 #add url rules for endpoints
 main.add_url_rule(
     '/api/main/projects',
     view_func=projects_view
 )
-
-# @main.route('/projects', methods=['GET', 'POST'])
-# def getProjects():
-#     if request.method == 'POST':
-#         title = request.json['title']
-        
-#         if title:
-#             existing_project = Project.query.filter_by(title = title).first()
-                        
-#             if existing_project is None:
-#                 project = Project(title=title)
-#                 project.save()
-                
-#                 res = jsonify({
-#                     'id': project.id,
-#                     'title': project.title,
-#                     'created_on': project.created_on,
-#                     'modified_on': project.modified_on 
-#                 })
-#                 res.status_code = 201
-#                 return res
-#             else:
-#                 data = {'message': 'Project already exists!'}
-#                 return make_response(jsonify(data), 409)
-#         else:
-#             data = {'message': 'Title is required!'}
-#             return make_response(jsonify(data), 400)
-#     else:
-#         projects = Project.get_all()
-#         results = []
-        
-#         for project in projects:
-#             obj = {
-#                 'id': project.id,
-#                 'title': project.title,
-#                 'created_on': project.created_on,
-#                 'modified_on': project.modified_on
-#             }
-#             results.append(obj)
-        
-#         res = jsonify(results)
-#         res.status_code = 200
-#         return res
-    
-@main.route('/projects/project/<int:id>', methods=['GET'])
-def get_project_by_id(id):
-    project = Project.query.filter_by(id = id).first()
-    
-    if not project:
-        abort(404)
-        
-    res = jsonify({
-        'id': project.id,
-        'title': project.title,
-        'created_on': project.created_on,
-        'modified_on': project.modified_on
-    })
-    res.status_code = 200
-    return res
-
-
-@main.route('/projects/project/<int:id>/update', methods=['PUT'])
-def update_project(id):
-    project = Project.query.filter_by(id = id).first()
-    
-    if not project:
-        abort(404)
-        
-    if request.method == 'PUT':
-        project.title = request.json['title']
-        project.save()
-        
-        res = jsonify({
-            'id': project.id,
-            'title': project.title,
-            'created_on': project.created_on
-        })
-        res.status_code = 200
-        return res
-
-@main.route('/projects/delete', methods=['DELETE'])
-def delete_project():
-    if request.method == 'DELETE':
-        ids = request.json['selectedIDs']
-        data = Project.delete(ids)
-    
-        return make_response(jsonify(data), 200)
+main.add_url_rule(
+    '/api/main/projects/project/<int:id>',
+    view_func=single_project_view
+)
     
 
 @main.route('/bugs', methods=['GET', 'POST'])
