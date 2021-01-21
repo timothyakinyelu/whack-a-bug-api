cf13b60bc3f784be55c14766f786ab089008cdab
loverofMush <tolujamal1@gmail.com>
2021-01-21 17:35:35 +0100

    added to baseCase - register_user - login_user - create_project

--- whack_a_bug_api/tests/BaseCase.py
+++ whack_a_bug_api/tests/BaseCase.py
@@ -2,6 +2,7 @@ from flask_testing import TestCase
 from whack_a_bug_api.helpers.load_config import loadConfig
 from whack_a_bug_api.db import db
 from whack_a_bug_api import createApp
+from flask import json
 
 app = createApp()
 
@@ -25,3 +26,35 @@ class BaseCase(TestCase):
         with self.app.app_context():
             db.session.remove()
             db.drop_all()
+            
+    def register_user(self):
+        """create new user for test cases"""
+        
+        self.user = {
+            'first_name': 'Juniper',
+            'last_name': 'Lee',
+            'email': 'lee.juniper@example.com',
+            'password': 'Jumper1'
+        }
+        
+        response = self.client.post('/api/auth/register', data = json.dumps(self.user), content_type = 'application/json')
+        return response
+    
+    def login_user(self):
+        """login a test case user"""
+        
+        self.user = {
+            'email': 'lee.juniper@example.com',
+            'password': 'Jumper1'
+        }
+        
+        response = self.client.post('/api/auth/login', data = json.dumps(self.user), content_type = 'application/json')
+        return response
+    
+    def create_project(self, headers):
+        """create a test case project"""
+        
+        self.project = {'title': 'Food Blog Design'}
+        
+        response = self.client.post('/api/main/projects', data = json.dumps(self.project), headers = headers)
+        return response
