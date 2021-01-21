7c7d17d6fbee8edcfa94708fcb2ad1247d5098e9
loverofMush <tolujamal1@gmail.com>
2021-01-21 12:44:17 +0100

    - created pivot table for projects and users

---
+++ whack_a_bug_api/models/pivots.py
@@ -0,0 +1,14 @@
+from  whack_a_bug_api.db import db
+
+project_user_table = db.Table('project_user', db.Model.metadata,
+    db.Column(
+        'project_id',
+        db.Integer,
+        db.ForeignKey('projects.id')
+    ),   
+    db.Column(
+        'users.id',
+        db.Integer,
+        db.ForeignKey('users.id')
+    )                           
+)
