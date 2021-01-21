b7e0ad94a7613d098864d19f05cc9164e12cc55c
loverofMush <tolujamal1@gmail.com>
2021-01-21 21:01:35 +0100

    - refactored bug tests

--- whack_a_bug_api/tests/bugs/test_bug.py
+++ whack_a_bug_api/tests/bugs/test_bug.py
@@ -4,17 +4,32 @@ from whack_a_bug_api.models.projects import Project
 from whack_a_bug_api.models.bugs import Bug
 
 class BugTests(BaseCase):
-    def test_bug_issue_can_be_created(self):
-        project = Project(title = 'Food Blog')
-        project.save()
-        
+    """Bug issue test units"""
+    
+    def create_bug(self, headers):
         self.bug = {
             'title': 'Unable to login',
-            'project_name': 'Food Blog'
+            'project_name': 'Food Blog Design'
         }
         
+        response = self.client.post('/api/main/bugs', data = json.dumps(self.bug), headers = headers)
+        return response
+        
+    def test_bug_issue_can_be_created(self):
+        self.register_user()
+        
         with self.client:
-            res = self.client.post('/api/main/bugs', data = json.dumps(self.bug), content_type = 'application/json')
+            login = self.login_user()
+            login_data = json.loads(login.data.decode())
+            
+            headers = {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer {}'.format(login_data['access_token'])
+            }
+            
+            self.create_project(headers)
+            
+            res = self.create_bug(headers)
             data = json.loads(res.data.decode())
             
             self.assertEqual(res.status_code, 201)
@@ -24,47 +39,45 @@ class BugTests(BaseCase):
             
             
     def test_all_issues_can_be_fetched(self):
-        project = Project(title = 'Food Blog')
-        project.save()
-        
-        self.bug = {
-            'title': 'Unable to login',
-            'project_name': 'Food Blog'
-        }
+        self.register_user()
         
         with self.client:
-            res = self.client.post('/api/main/bugs', data = json.dumps(self.bug), content_type = 'application/json')
-            self.assertEqual(res.status_code, 201)
+            login = self.login_user()
+            login_data = json.loads(login.data.decode())
             
-            resp = self.client.get('/api/main/bugs')
+            headers = {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer {}'.format(login_data['access_token'])
+            }
+            
+            self.create_project(headers)
+            
+            res = self.create_bug(headers)
+            
+            resp = self.client.get('/api/main/bugs', headers = headers)
             data = json.loads(resp.data.decode())
             
             self.assertEqual(resp.status_code, 200)
             self.assertIn('WB1001', data['data'][0]['ticket_ref'])
             
     def test_api_can_fetch_issue_by_id(self):
-        project = Project(title = 'Fashion Blog')
-        project.save()
-        
-        self.bug1 = {
-            'title': 'Unable to login',
-            'project_name': 'Fashion Blog'
-        }
-        
-        self.bug2 = {
-            'title': 'Cannot load dashboard',
-            'project_name': 'Fashion Blog'
-        }
+        self.register_user()
         
         with self.client:
-            res1 = self.client.post('/api/main/bugs', data = json.dumps(self.bug1), content_type = 'application/json')
-            res2 = self.client.post('/api/main/bugs', data= json.dumps(self.bug2), content_type = 'application/json')
+            login = self.login_user()
+            login_data = json.loads(login.data.decode())
             
-            self.assertEqual(res1.status_code, 201)
-            self.assertEqual(res2.status_code, 201)
+            headers = {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer {}'.format(login_data['access_token'])
+            }
             
-            res = self.client.get('/api/main/bugs/2')
+            self.create_project(headers)
+            
+            res = self.create_bug(headers)
+            
+            res = self.client.get('/api/main/bugs/1', headers = headers)
             data = json.loads(res.data.decode())
             
-            self.assertEqual('Cannot load dashboard', data['data']['title'])
+            self.assertEqual('Unable to login', data['data']['title'])
             self.assertEqual(res.status_code, 200)
